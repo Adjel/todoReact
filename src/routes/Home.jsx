32959d2d4af0a59ad5ef358832a1dbfe3dbead72
Adjel <adjel.touaoula@gmail.com>
2024-05-09 21:27:00 +0200

    feat(home): able to get todos and create a todo

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,16 +2,18 @@ import React, { useContext, useState } from "react";
 import styled from "styled-components"
 import { TodoContext } from "../../../providers/TodoProvider/TodoProvider";
 
-export default function Home() {
+export default function home() {
   const { todos, handleTodoInput, } = useContext(TodoContext);
-  const [title, setTile] = useState("")
+  const [title, setTitle] = useState("")
   return <Wrapper>
-    <div>{todos.map(({item}) => (
-      <div>{item}</div>
-    ))}</div>
+    <div>
+      {todos.map(({id, title, createdAt, completed}) => (
+        <div key={id}>{title}{completed}</div>
+      ))}
+    </div>
     <Form>
       <label htmlFor="title">Title:</label>
-      <input type="text" name="titme" id="title" required value={title} onChange={(event) => setTile(event.target.value)}></input>
+      <input type="text" name="title" id="title" required value={title} onChange={(event) => setTitle(event.target.value)}></input>
       <Button type="submit" onClick={(event) => setTodo(event, title)} >Create todo</Button>
     </Form>
   </Wrapper>;
