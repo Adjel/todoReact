6149b9ecf7f7f6bacc6c2abd3338b09a44694f17
Adjel <adjel.touaoula@gmail.com>
2024-05-09 15:24:09 +0200

    commit before trying to push data

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import styled from "styled-components"
 import { TodoContext } from "../../../providers/TodoProvider/TodoProvider";
 
 export default function Home() {
-  const { handleTodoInput, } = useContext(TodoContext);
-  return <Wrapper>Hello</Wrapper>;
+  const { todos, handleTodoInput, } = useContext(TodoContext);
+  const [title, setTile] = useState("")
+  return <Wrapper>
+    <div>{todos.map(({item}) => (
+      <div>{item}</div>
+    ))}</div>
+    <Form>
+      <label htmlFor="title">Title:</label>
+      <input type="text" name="titme" id="title" required value={title} onChange={(event) => setTile(event.target.value)}></input>
+      <button type="submit" onClick={(event) => setTodo(event, title)} >Create todo</button>
+    </Form>
+  </Wrapper>;
+
+  function setTodo(event, title) {
+    event.preventDefault();
+    handleTodoInput(title)
+  }
 }
 
 const Wrapper = styled.div``;
+
+const Form = styled.form``;
+
