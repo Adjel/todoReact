abb39f08658d071efc69d2097866df5c27ed8dc7
Adjel <adjel.touaoula@gmail.com>
2024-05-10 14:54:33 +0200

    environnement variables

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -11,7 +11,13 @@ import { collection, addDoc, serverTimestamp, onSnapshot } from 'firebase/firest
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
- 
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: "todoreact-6443e",
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID,
+  measurementId: import.meta.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase
